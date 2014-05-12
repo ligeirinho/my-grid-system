791b72a00c11a5cc74ec79d08629440840f5b1ae
Tiago Porto <tiagoporto@ymail.com>
2014-05-12 18:57:32 -0300

    Update
    add styles to media queries

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,119 @@
-// Load plugins
-var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-	imagemin = require('gulp-imagemin'),
-	rename = require('gulp-rename'),
-	clean = require('gulp-clean'),
-	concat = require('gulp-concat'),
-	sass = require('gulp-sass'),
-	path = require('path'),
-	watch = require('gulp-watch'),
+/*
+	Gulp.js Config File
+	Version: 1.0.0
+	Author: Tiago Porto - http://www.tiagoporto.com
+	https://github.com/tiagoporto
+	Contact: me@tiagoporto.com
+*/
+
+//************************* Load dependencies ****************************//
+var		  gulp = require('gulp'),
+		uglify = require('gulp-uglify'),
+	  imagemin = require('gulp-imagemin'),
+		rename = require('gulp-rename'),
+		 clean = require('gulp-clean'),
+		concat = require('gulp-concat'),
+		notify = require('gulp-notify'),
+	   compass = require('gulp-compass'),
+		  path = require('path'),
+		 watch = require('gulp-watch'),
 	livereload = require('gulp-livereload'),
-	lr = require('tiny-lr'),
-	server = lr();
+			lr = require('tiny-lr'),
+		server = lr();
 
-// Path configs
-var	css_path  = 'project/css/**/*.css', // .css files
-	sass_path = 'src/stylesheets/**/*.scss', // .sass files
-	img_path  = 'src/images/**/*.{png,jpg,gif}'; // image files
+//***************************** Path configs *****************************//
+var	   public_path = 'project/', // public files
+	 public_images = public_path + 'img', // optimized images
+	 public_styles = public_path + 'css', // minified styles
+	public_scripts = public_path + 'js', // concat and minify scripts
+		 sass_path = 'src/stylesheets/', // sass files
+		 js_path   = 'src/scripts/', // js files
+		 img_path  = 'src/images/'; // original image files
 
 
-//**********************************Tasks**********************************//
+//******************************** Tasks *********************************//
 
 // Optimize Images
 gulp.task('images', function() {
-	gulp.src([img_path, '!src/images/icons/*' ])
-		.pipe(imagemin({ optimizationLevel: 3, progressive: true, cache: true }))
-		.pipe(gulp.dest('project/img'))
-		.pipe(livereload(server));
+	gulp.src([img_path + '**/*.{png,jpg,gif}', '!' + img_path + '/icons/*'])
+		.pipe(imagemin({optimizationLevel: 4, progressive: true, cache: true}))
+		.pipe(gulp.dest(public_images))
+		.pipe(livereload(server))
+		.pipe(notify({message: 'Images task complete'}));
 });
 
+// Concat and Minify Scripts
+gulp.task('scripts', function() {
+	gulp.src([js_path + '/libs/**',
+		      js_path + 'frameworks/**',
+		      js_path + 'plugins/**',
+		      js_path + 'onread/open_onread.js',
+		      js_path + '/**/*.js',
+		      js_path + 'onread/close_onread.js'])
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('project/js'))
+		.pipe(rename('main.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(public_scripts))
+		.pipe(livereload(server))
+		.pipe(notify({message: 'Scripts task complete'}));
+});
 
-gulp.task('sass', function () {
-    gulp.src([sass_path,  '!src/stylesheets/media_queries/*'])
-        .pipe(sass())
-        .pipe(gulp.dest('project/css'))
-        .pipe(livereload(server));
+// Compile Compass
+gulp.task('compass', function() {
+	gulp.src(sass_path + '**/*.{sass,scss}')
+		.pipe(compass({
+			project: path.join(__dirname, '/'),
+			css: public_styles,
+			sass: sass_path,
+			image: img_path,
+			style: 'compressed', //The output style for the compiled css. Nested, expanded, compact, or compressed.
+			comments: false,
+			relative: false,
+		}))
+		.pipe(livereload(server))
+		.pipe(notify({message: 'Compass task complete'}));
 });
 
 // Clean Directories
 gulp.task('clean', function() {
-	return gulp.src(['project/css', 'project/img'], {read: false})
-		.pipe(clean());
+	return gulp.src([public_styles,
+					 public_scripts,
+					 public_images], {read: false})
+		.pipe(clean())
+		.pipe(notify({message: 'Clean task complete'}));
 });
 
 // Reload Browser
 gulp.task('reload-browser', function() {
-	gulp.src('project/**/*.html')
-		.pipe(livereload(server));
+	gulp.src(public_path + '**/*.{html,php}')
+		.pipe(livereload(server))
+		.pipe(notify({message: 'Reload complete'}));
 });
 
-//Watch
+// Watch
 gulp.task('watch', function() {
 	//Listen on port 35729
 	server.listen(35729, function (err) {
 		if (err) return console.log(err);
 
-		// Watch .scss files
-		gulp.watch(sass_path, function(event) {
-			gulp.run('sass');
+		// Watch .js files
+		gulp.watch(js_path + '**/*.js', function(event) {
+			gulp.run('scripts');
+		});
+
+		// Watch sass files
+		gulp.watch(sass_path + '**/*.{sass,scss}', function(event) {
+			gulp.run('compass');
 		});
 
 		// Watch .jpg .png .gif files
-		gulp.watch(img_path, function(event) {
+		gulp.watch(img_path + '**/*.{png,jpg,gif}', function(event) {
 		  gulp.run('images');
 		});
 
 		//Watch .html .php Files
-		gulp.watch('project/**/*.{html,php}', function(){
+		gulp.watch(public_path + '**/*.{html,php}', function(){
 			gulp.run('reload-browser');
 		});
 
@@ -73,6 +121,6 @@ gulp.task('watch', function() {
 });
 
 // Default task
-gulp.task('default', ['clean', 'sass', 'images'], function() {
+gulp.task('default', ['clean', 'compass', 'scripts', 'images'], function() {
 	gulp.run('watch');
 });
